cf428f0ac940f13006b0623220d84255e582af59
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2025-05-14 13:48:29 +0600

    for loop with Class and obj

---
+++ MyName.java
@@ -0,0 +1,20 @@
+
+
+
+class FahimNur {
+    void fahimNur(int a) {
+
+        for (int b = 1; b <= a; b++) {
+
+            System.out.println("My Name Is FahimNur");
+        }
+
+    }
+
+}
+public class MyName {
+    public static void main(String[] args) {
+        FahimNur fahimNur = new FahimNur();
+        fahimNur.fahimNur(5);
+    }
+}
