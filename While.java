99ae930c033fa4c63731e3ab68e2d71c24accd04
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2025-05-14 20:55:38 +0600

    while loop with method

---
+++ While.java
@@ -0,0 +1,21 @@
+class Loop{
+
+    int a;
+    void loop(){
+        while(a<=10 ){
+
+            System.out.println(a+"Fahim is a Good Boy");
+            a++;
+        }
+    }
+}
+
+
+public class While {
+    public static void main(String[] args) {
+        Loop loop = new Loop();
+        loop.loop();
+
+
+    }
+}
