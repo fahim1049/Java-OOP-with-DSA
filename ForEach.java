773e1836fc75096bfbd06abfd1c4c5b2d7efa2a8
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2025-05-23 14:50:32 +0600

    Array of for each loo

---
+++ ForEach.java
@@ -0,0 +1,11 @@
+public class ForEach {
+    public static void main(String[] args) {
+
+        String [] names = {"Fahim","Abdul", "Rifat"};
+
+                for(String name:names){
+                    System.out.println(name);
+                }
+        System.out.println("Length of Array: " +names.length);
+    }
+}
